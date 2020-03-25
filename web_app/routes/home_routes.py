eb54648109f465000e4291953dc86bde88e2f994
Cooper Vos <coopervos1@gmail.com>
2020-03-24 21:54:31 -0500

    Added bootstrap and some styles

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -10,6 +10,10 @@ home_routes = Blueprint("home_routes", __name__)
 def index():
     return render_template("layout.html")
 
+@home_routes.route("/about")
+def about():
+    return render_template("about.html")
+
 @home_routes.route("/twitter")
 def twitter():
     return render_template("twitter.html", message="This is Twitter central. Either get a specific users tweets or display all past retrieved users and their tweets.")
