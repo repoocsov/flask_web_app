47a2d57f7aa2af6752e37bf246b1e81453a41457
Cooper Vos <coopervos1@gmail.com>
2020-03-26 12:58:02 -0500

    First push to heroku

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -2,16 +2,20 @@
 
 from flask import Flask
 from web_app.models import db, migrate
+import os
+from dotenv import load_dotenv
 
 from web_app.routes.home_routes import home_routes
 from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.admin_routes import admin_routes
 
+load_dotenv()
+DATABASE_URL = os.getenv("sqlite:///tweets.db", default="sqlite:///tweets.db")
 
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tweets.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -23,5 +27,5 @@ def create_app():
     return app
 
 if __name__ == "__main__":
+    app.config['DEBUG'] = False
     my_app = create_app()
-    my_app.run(debug=True)
